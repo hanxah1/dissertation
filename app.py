8435e05e28b0db99fccfda18fcad9af454b8942e
Hannah Goodhead <goodheadhf@cardiff.ac.uk>
2024-07-01 16:14:31 +0100

    adding static and templates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def do_something():
-    return "something"
+def index():
+    return render_template('home.html')
