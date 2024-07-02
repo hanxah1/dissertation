345a827538cca3d6c44474a47757e23666d150bd
Hannah Goodhead <goodheadhf@cardiff.ac.uk>
2024-07-02 16:31:18 +0100

    adding progress 02/07

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+import json
 
 app = Flask(__name__)
+
+DATABASE = 'locations.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def index():
     return render_template('home.html')
+
+
+@app.route("/tool")
+def toolPage():
+    conn = get_db_connection()
+    locations = conn.execute('SELECT * FROM locations').fetchall()
+    conn.close()
+    return render_template('tool.html', locations=locations)
---
+++ data.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS 'Locations';
+
+
+CREATE TABLE "locations" (
+"Postcode" TEXT,
+  "Email" TEXT,
+  "Name" TEXT,
+  "Hours" TEXT,
+  "Phone" TEXT,
+  "Activities" TEXT,
+  "Website" TEXT,
+  "Unnamed: 7" TEXT,
+  "Latitude " REAL,
+  "Longitude" REAL
+)
